01c621aca4a2819ea6e665feb6c5a5e484671089
Jett <jd.dev96@gmail.com>
2023-03-20 19:17:31 -0400

    added tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "./page.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
-	return <main className={styles.main}>Home</main>;
+	return (
+		<main>
+			<h1 className="text-lg py-5">Home</h1>
+		</main>
+	);
 }
