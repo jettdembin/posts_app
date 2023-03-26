a23f3998a3f0d553ba432b8b5da95f1dc3965d68
Jett <jd.dev96@gmail.com>
2023-03-26 19:09:23 -0400

    fetching session server side with getServerSession from next-auth

--- app/auth/Nav.tsx
+++ app/auth/Nav.tsx
@@ -1,7 +1,11 @@
 import Link from "next/link";
 import Login from "./Login";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "../../pages/api/auth/[...nextauth]";
 
 export default async function Nav() {
+	const session = await getServerSession(authOptions);
+	console.log(session);
 	return (
 		<nav className="flex justify-between items-center py-8">
 			<Link href={"/"}>
