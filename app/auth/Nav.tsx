63f654fd1fd81ab455e19e4a32418cc874995a6a
Jett <jd.dev96@gmail.com>
2023-03-26 19:22:48 -0400

    sign out functionality added with google image added

--- app/auth/Nav.tsx
+++ app/auth/Nav.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Login from "./Login";
 import { getServerSession } from "next-auth/next";
 import { authOptions } from "../../pages/api/auth/[...nextauth]";
+import Logged from "./Logged";
 
 export default async function Nav() {
 	const session = await getServerSession(authOptions);
@@ -12,7 +13,8 @@ export default async function Nav() {
 				<h1 className="font-bold text-lg">Send it.</h1>
 			</Link>
 			<ul className="flex items-center gap-6">
-				<Login />
+				{!session?.user && <Login />}
+				{session?.user && <Logged image={session?.user?.image || ""} />}
 			</ul>
 		</nav>
 	);
