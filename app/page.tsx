ee589fbf1b1649cefd501e475d6d074c9fe5e74d
Jett <jd.dev96@gmail.com>
2023-03-27 19:28:47 -0400

    addposts component added to home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+"use client";
+import AddPosts from "./components/AddPosts";
+
 export default function Home() {
 	return (
 		<main>
 			<h1 className="text-lg py-5">Home</h1>
+			<AddPosts />
 		</main>
 	);
 }
