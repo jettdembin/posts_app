1ee01369c30cadd4390db8f15ce011c9be27bab1
Jett <jd.dev96@gmail.com>
2023-04-12 20:18:48 -0400

    added animation to posts when user gets to dashboard page

--- app/dashboard/EditPost.tsx
+++ app/dashboard/EditPost.tsx
@@ -1,10 +1,12 @@
 "use client";
-import { useState } from "react";
+
 import Image from "next/image";
+import { useState } from "react";
 import Toggle from "./Toggle";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 import axios from "axios";
-import { toast } from "react-hot-toast";
+import { motion } from "framer-motion";
 
 type EditProps = {
 	id: string;
@@ -27,36 +29,37 @@ export default function EditPost({
 }: EditProps) {
 	const [toggle, setToggle] = useState(false);
 	const queryClient = useQueryClient();
-	let deleteToastId: string;
+	let deleteToastID: string;
 
-	//delete post
 	const { mutate } = useMutation(
 		async (id: string) =>
-			await axios.delete(`/api/posts/deletePost`, { data: id }),
+			await axios.delete("/api/posts/deletePost", { data: id }),
 		{
 			onError: (error) => {
 				console.log(error);
-				toast.error("Error deleting that post", { id: deleteToastId });
 			},
 			onSuccess: (data) => {
 				console.log(data);
 				queryClient.invalidateQueries("getAuthPosts");
-				deleteToastId = toast.success("Post has been deleted.", {
-					id: deleteToastId,
-				});
+				toast.dismiss();
+				toast.success("Post has been deleted.");
 			},
 		}
 	);
 
 	const deletePost = () => {
-		deleteToastId = toast.loading("Deleting your post", {
-			id: deleteToastId,
-		});
+		deleteToastID = toast.loading("Deleting your post.");
 		mutate(id);
 	};
+
 	return (
 		<>
-			<div className="bg-white my-8 p-8 rounded-lg">
+			<motion.div
+				animate={{ opacity: 1, scale: 1 }}
+				initial={{ opacity: 0, scale: 0.8 }}
+				transition={{ ease: "easeOut" }}
+				className="bg-white my-8 p-8 rounded-lg "
+			>
 				<div className="flex items-center gap-2">
 					<Image width={32} height={32} src={avatar} alt="avatar" />
 					<h3 className="font-bold text-gray-700">{name}</h3>
@@ -78,8 +81,8 @@ export default function EditPost({
 						Delete
 					</button>
 				</div>
-			</div>
-			{toggle && <Toggle setToggle={setToggle} deletePost={deletePost} />}
+			</motion.div>
+			{toggle && <Toggle deletePost={deletePost} setToggle={setToggle} />}
 		</>
 	);
 }
